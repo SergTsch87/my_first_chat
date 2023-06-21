a9e8ad39418244c3cc34d989854f8bc88f715071
unknown <sergtsch87@gmail.com>
2023-06-21 19:17:23 +0300

    Binding the host and port

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import socket
 import sys
 import time
 
+# Створимо сокет та отримаємо ім'я хосту
 new_socket = socket.socket()
 host_name = socket.gethostname()
 s_ip = socket.gethostbyname(host_name)
 port = 8080
+
+# Зв’яжемо разом порт і хост
+new_socket.bind((host_name, port))
+print("Binding successfull!")
+print(f"This is your IP: {s_ip}")
